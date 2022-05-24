ca2b8134c34de978715b33c8754fbfc1c730288f
Aufa Billah <aufaroot18@gmail.com>
2022-05-25 00:29:50 +0700

    refactor: add theme provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Layout from "./layouts";
 import Routing from "./routes";
+import Modal from "react-modal";
+import { ThemeProvider } from "styled-components";
+import theme from "./utils/constants/theme";
+
+Modal.setAppElement("#root");
 
 function App() {
   return (
     <>
-      <Layout>
-        <Routing />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Routing />
+        </Layout>
+      </ThemeProvider>
     </>
   );
 }
