4f89296c5e33aeb2f29e4a2c21adce84e331394c
Aufa Billah <aufaroot18@gmail.com>
2022-05-25 07:42:05 +0700

    refactor: add margin props

--- src/components/ui/Paragraph/index.js
+++ src/components/ui/Paragraph/index.js
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 
 const Paragprah = styled.p`
-  color: ${({ theme }) => theme.colors.gray};
+  font-size: ${({ size }) => size && `${size}rem`};
+  color: ${({ color, theme }) => theme.colors[color] || theme.colors.gray};
+
+  margin-bottom: ${({ mb }) => mb && `${mb}rem`};
+  margin-top: ${({ mt }) => mt && `${mt}rem`};
+  margin-right: ${({ mr }) => mr && `${mr}rem`};
+  margin-left: ${({ ml }) => ml && `${ml}rem`};
 `;
 
 export default Paragprah;
