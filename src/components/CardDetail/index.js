f82219847b14e47fd75cb5e502008184ee7b3b56
Aufa Billah <aufaroot18@gmail.com>
2022-05-25 12:24:31 +0700

    refactor: handle error page

--- src/components/CardDetail/index.js
+++ src/components/CardDetail/index.js
@@ -14,11 +14,14 @@ import Select from "../ui/Select";
 import channels from "../../utils/constants/channels";
 import categories from "../../utils/constants/categories";
 import StyledCardDetail from "./Index.styled";
+import Error from "../../pages/error";
+import Header from "../Header";
 
 export default function CardDetail() {
   const { id } = useParams();
+  const [error, setError] = useState(false);
   const [modal, setModal] = useState(false);
-  const [product, setProduct] = useState({});
+  const [product, setProduct] = useState("");
   const [formData, setFormData] = useState({
     name: "",
     url: "",
@@ -27,6 +30,10 @@ export default function CardDetail() {
     category: "",
     sales_channel: "",
   });
+  const headerProps = {
+    title: "Toko Berhasil Dibuat!",
+    description: "Berikut adalah detail data toko anda",
+  };
 
   useEffect(() => {
     getDetailProduct();
@@ -35,16 +42,21 @@ export default function CardDetail() {
 
   async function getDetailProduct() {
     const url = `https://6285e14df0e8f0bb7c0b0de3.mockapi.io/api/v1/store/${id}`;
-    const { data } = await axios(url);
-    setProduct(data);
-    setFormData({
-      name: data.name,
-      url: data.url,
-      address: data.address,
-      postal: data.postal,
-      category: data.category,
-      sales_channel: data.sales_channel,
-    });
+
+    try {
+      const { data } = await axios(url);
+      setProduct(data);
+      setFormData({
+        name: data.name,
+        url: data.url,
+        address: data.address,
+        postal: data.postal,
+        category: data.category,
+        sales_channel: data.sales_channel,
+      });
+    } catch (error) {
+      setError(true);
+    }
   }
 
   function toggleModal() {
@@ -68,141 +80,146 @@ export default function CardDetail() {
     toggleModal();
   }
 
+  if (error) return <Error />;
+
   return (
-    <>
-      <StyledCardDetail>
-        <Box className="card__header" mb="1">
-          <Box mr="1">
-            <Image src={storePhoto} alt={product.name} />
+    product && (
+      <>
+        <Header {...headerProps} />
+        <StyledCardDetail>
+          <Box className="card__header" mb="1">
+            <Box mr="1">
+              <Image src={storePhoto} alt={product.name} />
+            </Box>
+            <Box>
+              <Heading as="h3">{product.name}</Heading>
+              <a href={product.url}>{product.url}</a>
+            </Box>
           </Box>
-          <Box>
-            <Heading as="h3">{product.name}</Heading>
-            <a href={product.url}>{product.url}</a>
+          <Box p="1">
+            <Box mb="1">
+              <Paragprah size="0.7" mb="0.5">
+                Detail Alamat
+              </Paragprah>
+              <Paragprah color="black">{product.address}</Paragprah>
+            </Box>
+            <Box mb="1">
+              <Paragprah size="0.7" mb="0.5">
+                Kode POS
+              </Paragprah>
+              <Paragprah color="black">{product.postal}</Paragprah>
+            </Box>
+            <Box mb="1">
+              <Paragprah size="0.7" mb="0.5">
+                Kategori Bisnis
+              </Paragprah>
+              <Paragprah color="black">{product.category}</Paragprah>
+            </Box>
+            <Box mb="1">
+              <Paragprah size="0.7" mb="0.5">
+                Saluran Penjualan
+              </Paragprah>
+              <Paragprah color="black">{product.sales_channel}</Paragprah>
+            </Box>
           </Box>
-        </Box>
-        <Box p="1">
-          <Box mb="1">
-            <Paragprah size="0.7" mb="0.5">
-              Detail Alamat
-            </Paragprah>
-            <Paragprah color="black">{product.address}</Paragprah>
-          </Box>
-          <Box mb="1">
-            <Paragprah size="0.7" mb="0.5">
-              Kode POS
-            </Paragprah>
-            <Paragprah color="black">{product.postal}</Paragprah>
-          </Box>
-          <Box mb="1">
-            <Paragprah size="0.7" mb="0.5">
-              Kategori Bisnis
-            </Paragprah>
-            <Paragprah color="black">{product.category}</Paragprah>
-          </Box>
-          <Box mb="1">
-            <Paragprah size="0.7" mb="0.5">
-              Saluran Penjualan
-            </Paragprah>
-            <Paragprah color="black">{product.sales_channel}</Paragprah>
-          </Box>
-        </Box>
-      </StyledCardDetail>
-      <Button onClick={toggleModal} type="outline">
-        Ubah
-      </Button>
-      <Modal isOpen={modal} onRequestClose={toggleModal}>
-        <Heading as="h2" mb="1" align="center">
-          Ubah Data Toko
-        </Heading>
-        <Heading as="h3" mb="1">
-          Data Toko
-        </Heading>
-        <form onSubmit={updateProduct}>
-          <Box mb="1">
-            <Label htmlFor="name">Nama Toko</Label>
-            <Input
-              type="text"
-              name="name"
-              id="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </Box>
-          <Box mb="1">
-            <Label htmlFor="url">Url Toko Online</Label>
-            <Input
-              type="text"
-              name="url"
-              id="url"
-              value={formData.url}
-              onChange={handleChange}
-              required
-            />
-          </Box>
-          <Box mb="1">
-            <Label htmlFor="address">Detail Alamat</Label>
-            <Input
-              as="textarea"
-              type="text"
-              name="address"
-              id="address"
-              value={formData.address}
-              onChange={handleChange}
-              required
-            />
-          </Box>
-          <Box mb="2">
-            <Label htmlFor="postal">Kode Pos</Label>
-            <Input
-              type="number"
-              name="postal"
-              id="postal"
-              value={formData.postal}
-              onChange={handleChange}
-              required
-            />
-          </Box>
-          <Heading as="h3" mb="1">
-            Informastion Bisnis
+        </StyledCardDetail>
+        <Button onClick={toggleModal} type="outline">
+          Ubah
+        </Button>
+        <Modal isOpen={modal} onRequestClose={toggleModal}>
+          <Heading as="h2" mb="1" align="center">
+            Ubah Data Toko
           </Heading>
-          <Box mb="1">
-            <Label htmlFor="category">category Bisnis</Label>
-            <Select
-              name="category"
-              id="category"
-              onChange={handleChange}
-              value={formData.category}
-            >
-              {categories.map((item) => {
-                return (
-                  <option value={item} key={item}>
-                    {item}
-                  </option>
-                );
-              })}
-            </Select>
-          </Box>
-          <Box mb="1">
-            <Label htmlFor="sales_channel">Channel Penjualan</Label>
-            <Select
-              name="sales_channel"
-              id="sales_channel"
-              onChange={handleChange}
-              value={formData.sales_channel}
-            >
-              {channels.map((item) => {
-                return (
-                  <option value={item} key={item}>
-                    {item}
-                  </option>
-                );
-              })}
-            </Select>
-          </Box>
-          <Button>Simpan</Button>
-        </form>
-      </Modal>
-    </>
+          <Heading as="h3" mb="1">
+            Data Toko
+          </Heading>
+          <form onSubmit={updateProduct}>
+            <Box mb="1">
+              <Label htmlFor="name">Nama Toko</Label>
+              <Input
+                type="text"
+                name="name"
+                id="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </Box>
+            <Box mb="1">
+              <Label htmlFor="url">Url Toko Online</Label>
+              <Input
+                type="text"
+                name="url"
+                id="url"
+                value={formData.url}
+                onChange={handleChange}
+                required
+              />
+            </Box>
+            <Box mb="1">
+              <Label htmlFor="address">Detail Alamat</Label>
+              <Input
+                as="textarea"
+                type="text"
+                name="address"
+                id="address"
+                value={formData.address}
+                onChange={handleChange}
+                required
+              />
+            </Box>
+            <Box mb="2">
+              <Label htmlFor="postal">Kode Pos</Label>
+              <Input
+                type="number"
+                name="postal"
+                id="postal"
+                value={formData.postal}
+                onChange={handleChange}
+                required
+              />
+            </Box>
+            <Heading as="h3" mb="1">
+              Informastion Bisnis
+            </Heading>
+            <Box mb="1">
+              <Label htmlFor="category">category Bisnis</Label>
+              <Select
+                name="category"
+                id="category"
+                onChange={handleChange}
+                value={formData.category}
+              >
+                {categories.map((item) => {
+                  return (
+                    <option value={item} key={item}>
+                      {item}
+                    </option>
+                  );
+                })}
+              </Select>
+            </Box>
+            <Box mb="1">
+              <Label htmlFor="sales_channel">Channel Penjualan</Label>
+              <Select
+                name="sales_channel"
+                id="sales_channel"
+                onChange={handleChange}
+                value={formData.sales_channel}
+              >
+                {channels.map((item) => {
+                  return (
+                    <option value={item} key={item}>
+                      {item}
+                    </option>
+                  );
+                })}
+              </Select>
+            </Box>
+            <Button>Simpan</Button>
+          </form>
+        </Modal>
+      </>
+    )
   );
 }
--- src/pages/product/detail.js
+++ src/pages/product/detail.js
@@ -1,16 +1,9 @@
 import CardDetail from "../../components/CardDetail";
-import Header from "../../components/Header";
 import Section from "../../components/ui/Section";
 
 export default function DetailProduct() {
-  const headerProps = {
-    title: "Toko Berhasil Dibuat!",
-    description: "Berikut adalah detail data toko anda",
-  };
-
   return (
     <Section>
-      <Header {...headerProps} />
       <CardDetail />
     </Section>
   );
